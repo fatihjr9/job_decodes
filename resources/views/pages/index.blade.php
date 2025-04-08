cea64bc2d62a11eaa7e3b3f87f16193e0280749f
Fatihjr9 <malikfatihibrahim@gmail.com>
2025-04-08 15:29:16 +0700

    add conditional : Done

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,9 +6,15 @@
     @endif
 
     <x-banner-job />
-    <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 my-4">
-        @foreach ($jobs as $job)
-            <x-job-card :job="$job" />
-        @endforeach
-    </div>
+    @if ($jobs->isEmpty())
+        <div class="col-span-2 lg:col-span-4 mt-8">
+            <p class="text-center text-gray-500">No jobs available at the moment.</p>
+        </div> 
+    @else
+        <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 my-4">
+            @foreach ($jobs as $job)
+                <x-job-card :job="$job" />
+            @endforeach
+        </div>
+    @endif
 </x-client-layout>
